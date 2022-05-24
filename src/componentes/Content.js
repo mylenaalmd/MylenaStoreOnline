fdfe7b89744de3b0c9a7f340a6cbeefff4b02e02
Mylena Nunes <mylenanunesdealmeida112@gmail.com>
2022-05-24 16:49:19 -0300

    atualizando erros de lint

--- src/componentes/Content.js
+++ src/componentes/Content.js
@@ -1,14 +1,15 @@
-import React from "react";
-import { Switch, Route} from "react-router-dom";
-import Home from "../pages/Home";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from '../pages/Home';
 
-class Content extends React.Component{
-  render(){
-    return(
+class Content extends React.Component {
+  render() {
+    return (
       <Switch>
-        <Route exact path='/' component={Home} />
+        <Route exact path="/" component={ Home } />
       </Switch>
-    )
+    );
   }
 }
+
 export default Content;
