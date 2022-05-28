c27594475f64d3e08fb7804e1aa5091f45b2dd7f
Flávia Herculano <flaviacunha18@hotmail.com>
2022-05-27 21:07:02 -0300

    Resolvendo o requisito 11

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -7,6 +7,11 @@ class Details extends React.Component {
       product: {},
       productList: [],
       redirect: false,
+      inputRating: 0,
+      email: '',
+      mensagem: '',
+      evaluations: [],
+      hasEvaluations: false,
     }
 
     async componentDidMount() {
@@ -16,6 +21,18 @@ class Details extends React.Component {
       const data = await response.json();
       this.setState({
         product: data,
+      });
+      this.getLocalStorageEvaluations();
+    }
+
+    getLocalStorageEvaluations = () => {
+      const { evaluations } = this.state;
+      if (evaluations[0]) {
+        const evaluationsList = JSON.parse(localStorage.getItem('evaluations'));
+        return this.setState({ evaluations: evaluationsList, hasEvaluations: true });
+      }
+      this.setState({
+        evaluations: [{ email: '', mensagem: '', inputRating: '' }],
       });
     }
 
@@ -31,8 +48,37 @@ class Details extends React.Component {
       localStorage.setItem('productId', JSON.stringify(productList));
     }
 
+    handleChange = (event) => {
+      console.log(event.target.value);
+      const { target: { value, checked, name } } = event;
+      const valor = name === 'email' || name === 'mensagem' ? value : checked;
+      if (checked) {
+        return this.setState({
+          [name]: value,
+        });
+      }
+      this.setState({
+        [name]: valor,
+      });
+    }
+
+    handleSubmit = (e) => {
+      const { email, mensagem, inputRating, evaluations } = this.state;
+      e.preventDefault();
+      const obj = { email, mensagem, inputRating };
+      const arr = [...evaluations, obj];
+      this.setState({
+        evaluations: arr,
+        hasEvaluations: true,
+        inputRating: 0,
+        email: '',
+        mensagem: '',
+      }, () => localStorage.setItem('evaluations', JSON.stringify(arr)));
+    }
+
     render() {
-      const { product: { price, thumbnail, title }, redirect } = this.state;
+      const { product: { price, thumbnail, title }, redirect,
+        email, mensagem, evaluations, hasEvaluations } = this.state;
       if (redirect) {
         return (
           <Redirect to="/cart" />
@@ -58,6 +104,118 @@ class Details extends React.Component {
             >
               Carrinho de compras
             </button>
+          </div>
+          <div>
+            <h3>Avaliação</h3>
+            <form>
+              <label htmlFor="e-mail">
+                <input
+                  type="email"
+                  value={ email }
+                  name="email"
+                  data-testid="product-detail-email"
+                  placeholder="E-mail"
+                  onChange={ this.handleChange }
+                />
+                <br />
+                <input
+                  type="radio"
+                  value="1"
+                  name="inputRating"
+                  data-testid="1-rating"
+                  onChange={ this.handleChange }
+                />
+                <input
+                  type="radio"
+                  value="2"
+                  name="inputRating"
+                  data-testid="2-rating"
+                  onChange={ this.handleChange }
+                />
+                <input
+                  type="radio"
+                  value="3"
+                  name="inputRating"
+                  data-testid="3-rating"
+                  onChange={ this.handleChange }
+                />
+                <input
+                  type="radio"
+                  value="4"
+                  name="inputRating"
+                  data-testid="4-rating"
+                  onChange={ this.handleChange }
+                />
+                <input
+                  type="radio"
+                  value="5"
+                  name="inputRating"
+                  data-testid="5-rating"
+                  onChange={ this.handleChange }
+                />
+              </label>
+              <label htmlFor="e-mail">
+                <textarea
+                  type="text"
+                  value={ mensagem }
+                  name="mensagem"
+                  placeholder="Mensagem (opcional)"
+                  data-testid="product-detail-evaluation"
+                  onChange={ this.handleChange }
+                />
+              </label>
+              <br />
+              <button
+                type="submit"
+                data-testid="submit-review-btn"
+                onClick={ this.handleSubmit }
+              >
+                Avaliar
+              </button>
+            </form>
+            {hasEvaluations && (
+              <div className="evaluations">
+                {hasEvaluations && evaluations.map((evaluation) => (
+                  <div key={ evaluation.email }>
+                    <input
+                      type="radio"
+                      value="1"
+                      name={ `inputRated${evaluation.email}` }
+                      checked={ evaluation.inputRating === '1' }
+                    />
+                    <input
+                      type="radio"
+                      value="2"
+                      name={ `inputRated${evaluation.email}` }
+                      checked={ evaluation.inputRating === '2' }
+                    />
+                    <input
+                      type="radio"
+                      value="3"
+                      name={ `inputRated${evaluation.email}` }
+                      checked={ evaluation.inputRating === '3' }
+                    />
+                    <input
+                      type="radio"
+                      value="4"
+                      name={ `inputRated${evaluation.email}` }
+                      checked={ evaluation.inputRating === '4' }
+                    />
+                    <input
+                      type="radio"
+                      value="5"
+                      name={ `inputRated${evaluation.email}` }
+                      checked={ evaluation.inputRating === '5' }
+                    />
+                    <p>
+                      { evaluation.email }
+                    </p>
+                    <p>
+                      { evaluation.mensagem }
+                    </p>
+                  </div>
+                ))}
+              </div>)}
           </div>
         </div>
       );
